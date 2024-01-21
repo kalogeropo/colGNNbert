d2d671a178ab5be72039660b0391432a8af1250b
Agorakis Bompotas <mpompotas@ceid.upatras.gr>
2024-01-21 16:14:35 +0200

    changes for training/evaluating the CNN for ranking

--- colbert/nn/cnn_ranker.py
+++ colbert/nn/cnn_ranker.py
@@ -1,11 +1,10 @@
-from torch import flatten
-from torch import float as tfloat
-from torch.nn import NLLLoss
+import torch
 from torch.nn import Conv2d
 from torch.nn import Linear
 from torch.nn import LogSoftmax
 from torch.nn import MaxPool2d
 from torch.nn import Module
+from torch.nn import NLLLoss
 from torch.nn import ReLU
 from torch.optim import Adam
 
@@ -16,87 +15,120 @@ class CNNModel(Module):
     def __init__(self):
         # call the parent constructor
         super(CNNModel, self).__init__()
+
         # initialize first set of CONV => RELU => POOL layers
         self.conv1 = Conv2d(in_channels=1, out_channels=20, kernel_size=(5, 5))
         self.relu1 = ReLU()
         self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
         # initialize second set of CONV => RELU => POOL layers
         self.conv2 = Conv2d(in_channels=20, out_channels=50, kernel_size=(5, 5))
         self.relu2 = ReLU()
         self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
         # initialize first (and only) set of FC => RELU layers
         self.fc1 = Linear(in_features=800, out_features=500)
         self.relu3 = ReLU()
+
         # initialize our softmax classifier
         self.fc2 = Linear(in_features=500, out_features=2)
         self.logSoftmax = LogSoftmax(dim=1)
 
     def forward(self, x):
-        # pass the input through our first set of CONV => RELU =>
-        # POOL layers
+        # pass the input through our first set of CONV => RELU => POOL layers
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.maxpool1(x)
-        # pass the output from the previous layer through the second
-        # set of CONV => RELU => POOL layers
+
+        # pass the output from the previous layer through the second set of CONV => RELU => POOL layers
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.maxpool2(x)
-        # flatten the output from the previous layer and pass it
-        # through our only set of FC => RELU layers
-        x = flatten(x, 1)
+
+        # flatten the output from the previous layer and pass it through our only set of FC => RELU layers
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = self.relu3(x)
-        # pass the output to our softmax classifier to get our output
-        # predictions
+
+        # pass the output to our softmax classifier to get our output predictions
         x = self.fc2(x)
         output = self.logSoftmax(x)
-        # return the output predictions
         return output
 
 
 class CNNRanker(NNRanker):
     def __init__(self, args):
         super(CNNRanker, self).__init__(args)
+        self.model = None
+        self.loss = None
+        self.optimizer = None
+
+    def fit(self, train_data_loader, val_data_loader):
         self.model = CNNModel()
         self.model.to('cpu')
-
-    def fit(self, similarities, labels):
         # initialize our optimizer and loss function
-        opt = Adam(self.model.parameters(), lr=self.args.lr)
-        lossFn = NLLLoss()
-        # initialize a dictionary to store training history
-        H = {
-            "train_loss": [],
-            "train_acc": [],
-            "val_loss": [],
-            "val_acc": []
-        }
+        self.optimizer = Adam(self.model.parameters(), lr=self.args.lr)
+        self.loss = NLLLoss()
 
         # loop over our epochs
         for e in range(0, self.args.epochs):
             # set the model in training mode
             self.model.train()
+
             # initialize the total training and validation loss
-            totalTrainLoss = 0
-            totalValLoss = 0
-            # initialize the number of correct predictions in the training
-            # and validation step
-            trainCorrect = 0
-            valCorrect = 0
-            # loop over the training set
-            for i in range(labels):
+            self.total_train_loss = 0
+            self.total_val_loss = 0
+
+            # initialize the number of correct predictions in the training and validation step
+            self.train_correct = 0
+            self.val_correct = 0
+
+            self._fit(train_data_loader)
+            self._validation(val_data_loader)
+
+    def _fit(self, train_data_loader):
+        # loop over the training set
+        for (x, y) in train_data_loader:
+            # send the input to the device
+            (x, y) = (x.to('cpu'), y.to('cpu'))
+
+            # perform a forward pass and calculate the training loss
+            pred = self.model(x)
+            loss = self.loss(pred, y)
+
+            # zero out the gradients, perform the backpropagation step and update the weights
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # add the loss to the total training loss so far and calculate the number of correct predictions
+            self.total_train_loss += loss
+            self.train_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    def _validation(self, val_data_loader):
+        # switch off autograd for evaluation
+        with torch.no_grad():
+            # set the model in evaluation mode
+            self.model.eval()
+
+            # loop over the validation set
+            for (x, y) in val_data_loader:
                 # send the input to the device
-                (x, y) = (similarities[i].to('cpu'), labels[i].to('cpu'))
-                # perform a forward pass and calculate the training loss
+                (x, y) = (x.to('cpu'), y.to('cpu'))
+
+                # make the predictions and calculate the validation loss
                 pred = self.model(x)
-                loss = lossFn(pred, y)
-                # zero out the gradients, perform the backpropagation step,
-                # and update the weights
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
-                # add the loss to the total training loss so far and
+                self.total_val_loss += self.loss(pred, y)
+
                 # calculate the number of correct predictions
-                totalTrainLoss += loss
-                trainCorrect += (pred.argmax(1) == y).type(tfloat).sum().item()
+                self.val_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    def predict(self, similarities, path=None):
+        if self.model is None:
+            self.model = NNRanker.load(path)
+            self.model.to('cpu')
+        # switch off autograd
+        with torch.no_grad():
+            x = similarities.to('cpu')
+            pred = self.model(x)
+            #return pred.argmax(axis=1).cpu().numpy()[0]
--- colbert/nn/nn_ranker.py
+++ colbert/nn/nn_ranker.py
@@ -1,47 +1,82 @@
-from torch import load as torch_load
-from torch import save as torch_save
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torch.utils.data import random_split
 
 from colbert.modeling.inference import ModelInference
 from colbert.utils.collection_parser import Collection
 
 
-class NNRanker:
-    def __init__(self, args):
-        self.args = args
-        self.inference = None
-        self.model = None
+class TripletsDataset(Dataset):
+    def __init__(self, collection, inference):
+        self.collection = collection
+        self.inference = inference
+        self.similarities, self.labels = self._parse_triplets()
 
-    def train(self, save=True, path=None):
-        self.inference = ModelInference(self.args.colbert, amp=self.args.amp)
-        collection = Collection(triplets_tsv=self.args.triples)
-        triplets = collection.create_set()
-
+    def _parse_triplets(self):
         def create_similarities_matrix(q, d):
             q = self.inference.queryFromText(q)
             d = self.inference.docFromText(d)
             return self.inference.scores_matrix(q, d)
 
-        similarities = triplets[['Query', 'Doc']].agg(create_similarities_matrix, axis='columns')
+        triplets = self.collection.create_set()
+        return triplets[['Query', 'Doc']].agg(create_similarities_matrix, axis='columns'), triplets['Pos/Neg']
 
-        self.fit(similarities, triplets['Pos/Neg'])
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.similarities.iloc[idx], self.labels[idx]
+
+
+class NNRanker:
+    def __init__(self, args):
+        self.args = args
+        self.collection = None
+        self.inference = None
+        self.model = None
+        self.train_split = 0.75
+        self.total_train_loss = 0
+        self.total_val_loss = 0
+        self.train_correct = 0
+        self.val_correct = 0
+
+    def train(self, save=True, path=None):
+        self.collection = Collection(triplets_tsv=self.args.triples)
+        self.inference = ModelInference(self.args.colbert, amp=self.args.amp)
+        triplets_dataset = TripletsDataset(self.collection, self.inference)
+
+        dataset_len = len(triplets_dataset)
+        train_len = int(dataset_len * self.train_split)
+        val_len = int(dataset_len * (1 - self.train_split))
+        (train_x, val_x) = random_split(triplets_dataset, [train_len, val_len],
+                                        generator=torch.Generator().manual_seed(42))
+
+        train_data_loader = DataLoader(train_x, shuffle=True, batch_size=self.args.bsize)
+        val_data_loader = DataLoader(val_x, batch_size=self.args.bsize)
+
+        train_steps = len(train_data_loader.dataset) // self.args.bsize
+        val_steps = len(val_data_loader.dataset) // self.args.bsize
+
+        self.fit(train_data_loader, val_data_loader)
         if save:
             self.save(path)
 
-    def fit(self, x, y):
+    def fit(self, train_loader, val_loader):
         pass
 
-    def predict(self, x):
+    def predict(self, x, path=None):
         pass
 
     def save(self, path=None):
         if path is None:
-            torch_save(self.model, './nn_ranker')
+            torch.save(self.model, './nn_ranker')
         else:
-            torch_save(self.model, path)
+            torch.save(self.model, path)
 
     @staticmethod
     def load(path=None):
         if path is None:
-            return torch_load('./nn_ranker')
+            return torch.load('./nn_ranker')
         else:
-            return torch_load(path)
+            return torch.load(path)
