acbd4ddca0c1da8d60b28f0649c0b354ad0ee3c5
Agorakis Bompotas <mpompotas@ceid.upatras.gr>
2024-02-06 17:37:33 +0200

    read and convert dataset for ml tasks

--- dataset2d/image_cnn.py
+++ dataset2d/image_cnn.py
@@ -1,77 +1,29 @@
 import ast
-from os import listdir, getcwd
-from os.path import isdir, join
 import torchvision.transforms as transforms
 
 import pandas as pd
 
 import torch
 from torch.nn import Module, Conv2d, ReLU, MaxPool2d, Linear, Softmax
-from torch.utils.data import Dataset, DataLoader
-from torch import nn
-from PIL import Image
-
-
-def split_dataset(dataset, ratio):
-    dataset_len = len(dataset)
-    train_len = int(dataset_len * ratio)
-    val_len = dataset_len - train_len
-    train_set, validation_set = torch.utils.data.random_split(dataset, [train_len, val_len],
-                                                              generator=torch.Generator().manual_seed(1234))
-    print(F"Train set size: {len(train_set)}\nTest set size: {len(validation_set)}")
-    return train_set, validation_set, train_len, val_len
-
-
-def read_set():
-    # Get image files paths
-    # dirs = [name for name in listdir(".") if isdir(name)]
-    # cwd = getcwd()
-    #
-    # image_dir = join(cwd, dirs[1])
-    #
-    # images = [join(image_dir, f) for f in listdir(image_dir)]
-    # images = sorted(images, key=len)
-
-    # Load images into lists
-
-    # img_vecs = []
-    # for path in images:
-    #     img = Image.open(path)
-    #     img_vecs.append(img)
-    # print(len(img_vecs))
-    # img_vecs[0].show()
-    with open('labels.csv') as f:
-        reader = f.readlines()
-        data = [int(i) for i in reader]
-    # print(len(data))
-    # Create dataframe of paths images and labels
-    # dict_to_df = {"img_path": images, "label": data, "img_vec": img_vecs}
-    # df = pd.DataFrame(dict_to_df)
-    # print(df.head())
-    image_tensors = pd.read_csv('tensors.csv')
-    # print(type(image_tensors['features'][0]))
-    features = image_tensors['features'].map(ast.literal_eval)
-    assert len(data) == len(image_tensors), f"image list and labels have different sizes"
-    return features, data, len(image_tensors)
+from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
-    def __init__(self, transform=transforms.ToTensor()):
-        self.transform = transform
-        self.tensors, self.labels, self.length = read_set()
-        print(f"Loaded {len(self.tensors)} images from ColBERT Dataset")
+    def __init__(self, path='tensors.csv'):
+        images_df = pd.read_csv(path)
+        self.features = images_df['features'].map(ast.literal_eval).map(torch.Tensor)
+        self.labels = images_df['label']
+        assert len(self.features) == len(self.labels), f"image list and labels have different sizes"
+        print(f"Loaded {len(self.features)} images from ColBERT Dataset")
 
     def __len__(self):
-        return self.length
+        return len(self.features)
 
     def __getitem__(self, index):
-        #image = self.transform(self.tensors[index])
-        image = self.tensors[index]
-        return image, self.labels[index]
+        return self.features[index], self.labels[index]
 
     def calc_init_w_h(self):
-        w, h = self.tensors[0].size
-        return w, h
+        return self.features[0].size
 
 
 class CNNModel(Module):
--- dataset2d/matrix_ml.py
+++ dataset2d/matrix_ml.py
@@ -1,11 +1,9 @@
+import torch
 from torch.nn import NLLLoss
 from torch.optim import Adam
-
-from image_cnn import CustomDataset, CNNModel, split_dataset
-
-import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
+
+from image_cnn import CustomDataset, CNNModel
 
 NUM_EPOCHS = 10
 TRAIN_SPLIT = 0.9
@@ -13,55 +11,63 @@ BSIZE = 32
 SHUFFLE = True
 LEARNING_RATE = 0.001
 
-# Initialize Data set
-image_custom_set = CustomDataset()
-w, h = image_custom_set.calc_init_w_h()
 
-# set the model in training mode
-train_set, validation_set, train_len, val_len = split_dataset(image_custom_set, TRAIN_SPLIT)
+def split_dataset(dataset, ratio):
+    dataset_len = len(dataset)
+    train_len = int(dataset_len * ratio)
+    val_len = dataset_len - train_len
+    train_set, validation_set = torch.utils.data.random_split(dataset, [train_len, val_len],
+                                                              generator=torch.Generator().manual_seed(1234))
+    print(F"Train set size: {len(train_set)}\nTest set size: {len(validation_set)}")
+    return train_set, validation_set, train_len, val_len
 
-train_loader = DataLoader(dataset=train_set, shuffle=SHUFFLE, batch_size=BSIZE)
-validation_loader = DataLoader(dataset=validation_set, shuffle=SHUFFLE, batch_size=BSIZE)
 
+if __name__ == '__main__':
+    # Initialize Data set
+    image_custom_set = CustomDataset()
+    w, h = image_custom_set.calc_init_w_h()
 
-train_steps = train_len // BSIZE
-val_steps = val_len // BSIZE
+    # set the model in training mode
+    train_set, validation_set, train_len, val_len = split_dataset(image_custom_set, TRAIN_SPLIT)
+    train_loader = DataLoader(dataset=train_set, shuffle=SHUFFLE, batch_size=BSIZE)
+    validation_loader = DataLoader(dataset=validation_set, shuffle=SHUFFLE, batch_size=BSIZE)
 
-print(train_steps, val_steps)
+    train_steps = train_len // BSIZE
+    val_steps = val_len // BSIZE
+    print(train_steps, val_steps)
 
-model = CNNModel(h, w)
+    model = CNNModel(h, w)
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+    loss = NLLLoss()
 
-optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
-loss = NLLLoss()
+    # loop over our epochs
+    for e in range(0, NUM_EPOCHS):
+        print(e)
+        # initialize the total training and validation loss
+        total_train_loss = 0
+        total_val_loss = 0
 
-# loop over our epochs
-for e in range(0, NUM_EPOCHS):
-    print(e)
-    # initialize the total training and validation loss
-    total_train_loss = 0
-    total_val_loss = 0
+        model.train()
+        # initialize the number of correct predictions in the training and validation step
+        train_correct = 0
+        val_correct = 0
+        for (x, y) in train_loader:
+            # send the input to the device
+            (x, y) = (x.to('cpu'), y.to('cpu'))
+            print(x, y)
+            # perform a forward pass and calculate the training loss
+            pred = model(x)
+            loss = loss(pred, y)
 
-    model.train()
-    # initialize the number of correct predictions in the training and validation step
-    train_correct = 0
-    val_correct = 0
-    for (x, y) in train_loader:
-        # send the input to the device
-        (x, y) = (x.to('cpu'), y.to('cpu'))
-        print(x, y)
-        # perform a forward pass and calculate the training loss
-        pred = model(x)
-        loss = loss(pred, y)
+            # zero out the gradients, perform the backpropagation step and update the weights
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        # zero out the gradients, perform the backpropagation step and update the weights
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # add the loss to the total training loss so far and calculate the number of correct predictions
+            total_train_loss += loss
+            train_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-        # add the loss to the total training loss so far and calculate the number of correct predictions
-        total_train_loss += loss
-        train_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-        # _fit(train_data_loader)
-        # self._validation(val_data_loader)
-        # self._training_info(e + 1)
+            # _fit(train_data_loader)
+            # self._validation(val_data_loader)
+            # self._training_info(e + 1)
